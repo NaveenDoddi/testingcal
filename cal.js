6c42da07ae52c59a2a0fd76b57fbc878f4b172da
naveendoddi <prasadnaveen847@gmail.com>
2023-02-27 20:22:27 +0530

    plzz commit

---
+++ cal.js
@@ -0,0 +1,55 @@
+function run(){
+    var data = document.getElementById("input").value
+    var solution = eval(data)
+    document.body.append(solution)
+}
+function sum(){
+
+    var data = document.getElementById("input1").innerText
+
+    document.getElementById("input1").innerText=data+"+"
+
+    
+}
+
+function ans(){
+    var data = document.getElementById("input1").innerText
+    document.getElementById("para").innerText = ""
+    document.getElementById("para").append(eval(data))
+    
+}
+
+
+
+
+function sub(){
+    var input1 = document.getElementById("input1").value
+    var input2 = document.getElementById("input2").value
+
+    var num1 = parseFloat(input1)
+    var num2 = parseFloat(input2)
+
+    document.getElementById("para").append(num1-num2)
+}
+function mul(){
+
+    var input1 = document.getElementById("input1").innerText
+    document.getElementById("para").append(eval(input1))
+
+}
+
+
+function push1(){
+    var data = document.getElementById("input1").innerText
+
+    var solution = data+"1"
+
+    document.getElementById("input1").innerText = solution
+
+    
+}
+function push2(){
+    var data = document.getElementById("input1").innerText
+
+    document.getElementById("input1").innerText=data+"2"
+}
