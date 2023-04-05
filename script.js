3cec3ce2deec93b0b8c584de09e723eee15ec9ca
naveendoddi <prasadnaveen847@gmail.com>
2023-04-05 22:29:55 +0530

    new

--- script.js
+++ script.js
@@ -1,319 +1,455 @@
 // function run(){
-//     var data = document.getElementById("input").value
-//     var solution = eval(data)
-//     document.body.append(solution)
-// }
-// function sum(){
-
-//     var data = document.getElementById("input1").innerText
-
-//     document.getElementById("input1").innerText=data+"+"
-
     
+//   let kothaDhi = {
+//     firstname : "naveen",
+//     lastname  : "doddi",
+//     DOB : 1111111,
+//     gender : "M",
+//     email : "prasadnaveen@123",
+//     username : "n",
+//     password : "n"
+//   }
+
+    // kothaDhi = JSON.stringify(kothaDhi)
+    // console.log(kothaDhi)
+
+// let naveen = [
+//   {
+//     0: "Elbertine",
+//     1: "Gissing",
+//     2: "egissing0@npr.org",
+//     4: "female",
+//     3: "$44802.77",
+//     5: "514 625 0311",
+//     6: 1,
+//     // "+7":"usha",
+//   }
+// ]
+
+// naveen.map(n)
+// console.log(n)
+// function n(item){
+    // console.log(item)
 // }
-
-// function ans(){
-//     var data = document.getElementById("input1").innerText
-//     document.getElementById("para").innerText = ""
-//     document.getElementById("para").append(eval(data))
-//     //yeayyy
-// }
-
-
-// function sub(){
-//     var input1 = document.getElementById("input1").value
-//     var input2 = document.getElementById("input2").value
-
-//     var num1 = parseFloat(input1)
-//     var num2 = parseFloat(input2)
-
-//     document.getElementById("para").append(num1-num2)
-// }
-// function mul(){
-
-//     var input1 = document.getElementById("input1").innerText
-//     document.getElementById("para").append(eval(input1))
-
-// }
-
-
-// function push1(){
-//     var data = document.getElementById("input1").innerText
-
-//     var solution = data+"1"
-
-//     document.getElementById("input1").innerText = solution
-
-    
-// }
-// function push2(){
-//     var data = document.getElementById("input1").innerText
-
-//     document.getElementById("input1").innerText=data+"2"
-// }
-
-
-
-
-var value;
-function run(){
-
-var namee = document.getElementById("f").innerText
-persons[0].name = namee
-persons = JSON.stringify(persons)
-fetch("https://reqres.in/api/users", {
-  method: "POST",
-  body: persons,
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-})
-  .then((response) => response.json())
-  
-  .then(function rrr(data){
-    console.log(data)
-    value = data
-    alert("fuck")
-  }); 
-var value1
-  // 
-  setTimeout(rrr, 1000);
-  
-  // fetch('https://reqres.in/api/users')      //api for the get request
-  // .then(response => response.json())
-  // .then(data => console.log(data));
-//     var name = document.getElementById("name").value
-//     var passcode = document.getElementById("passcode").value
-//     var para = document.createElement("p")
-//     para.innerText = passcode
-//     para.id = "para1"
-//     para.style.visibility="hidden"
-//     // para.id = "para1"
-
-//     var div = document.createElement("div")
-//     div.style.border="2px solid red"
-//     div.style.height="200px"
-//     div.style.width="200px"
-//     div.id="div1"
-//     div.append(name,para)
-//     // passcode.style.visibility="hidden"
-// // document.getElementById("passcode").style.visibility="hidden"
-//     var button = document.createElement("button")
-//     button.innerText= "created"
-//     // button.addEventListener("click",run1)
-//     button.onclick = run1
-//     div.append(button)
-//     document.body.append(div)
-
-
-// var table = document.createElement("table")
-// var row = document.createElement("tr")
-// var col = document.createElement("td")
-// var col1 = document.createElement("td")
-// var col2 = document.createElement("td")
-
-// col.innerText = "Name"
-// col1.innerText = "class"
-// col2.innerText = "age"
-// row.append(col,col1,col2)
-
-// var row1 = document.createElement("tr")
-// var col3= document.createElement("td")
-// var col4 = document.createElement("td")
-// // var col5 = document.createElement("td")
-// col3.innerText = "naveen"
-// col4.innerText = "10"
-// col4.setAttribute("colspan","2")
-// // col5.innerText = "12"
-// row1.append(col3,col4)
-// table.append(row,row1)
-
-// document.body.append(table)
-
-
-
-// var forms = document.getElementsByClassName('needs-validation');
-
-// var validation = Array.prototype.filter.call(forms, function(form) {
-//     form.addEventListener('submit', function(event) {
-//       if (form.checkValidity() === false) {
-//         event.preventDefault();
-//         // event.stopPropagation();
-//       }
-//       form.classList.add('was-validated');
-//     });
-//   });
-
-
-
-
-    
-}
-const empdata1to20 = [
-  {
-    name: "Elbertine",
-    1: "Gissing",
-    2: "egissing0@npr.org",
-    4: "female",
-    3: "$44802.77",
-    5: "514 625 0311",
-    6: 1,
-    "+7":"run",
-}
-]
-
-
-// console.log(empdata1to20[0][+7])
+//  var n = Object.keys(empdata1to20[0]).length
+// // console.log(empdata1to20[0][1])
+// // console.log(empdata1to20[0][+7])
 // for(let i=0; i<empdata1to20.length;i++){
-//     for(let j in empdata1to20[i]){
-//         console.log(empdata1to20[i][j])
+//     for(let j= 0; j<n; j++){
+//       console.log(empdata1to20[i][j])
 //     }
 // }
 
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //         'X-RapidAPI-Key': '754ee67f44msh73315c567c13cc6p163e85jsn2f883ac74a0c',
+  //         'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+  //     }
+  // };
+  
+  // fetch("https://deezerdevs-deezer.p.rapidapi.com/infos",options)
+  //     .then(response => response.json())
+  //     .then(data => console.log(data.ads.audio))
+  //     .catch(error => console.error(error));
 
-// var person = new Map(empdata1to20)
-// const persons = [
-//   {firstname : "Malcom", lastname: "Reynolds"},
-//   {firstname : "Kaylee", lastname: "Frye"},
-//   {firstname : "Jayne", lastname: "Cobb"},
-//   {
-//     firstname: "Elbertine",
-//     1: "Gissing",
-//     2: "egissing0@npr.org",
-//     4: "female",
-//     3: "$44802.77",
-//     5: "514 625 0311",
-//     6: 1,
-//     "+7":"run",
-//   },
-//   {
-//     firstname: "Elbertine",
-//     1: "Gissing",
-//     2: "egissing0@npr.org",
-//     4: "female",
-//     3: "$44802.77",
-//     5: "514 625 0311",
-//     6: 1,
-//     "+7":"run",
-//   },
-//   {
-//     firstname: "Elbertine",
-//     1: "Gissing",
-//     2: "egissing0@npr.org",
-//     4: "female",
-//     3: "$44802.77",
-//     5: "514 625 0311",
-//     6: 1,
-//     "+7":"run",
-//   },
-// ]
-let persons=[
-  {
-    three:3,
-    one:1,
-    four:4,
-    two:2,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // fetch("C:\Users\Doddi Naveen\Desktop\files\cal\testingcal\batabase.json", {
+    //   method: "POST",
+    //   body: kothaDhi,
+    //   headers: {
+    //     "Content-type": "application/json; charset=UTF-8"
+    //   }
+    // })
+    //   .then((response) => response.json())
+
+    //   // .then(rrr());
+    //   .then((data)=>console.log(data))
+    // console.log(promise.status)
     
-  }
-]
-persons.map(n)
-// console.log(n)
-function n(item){
-  // console.log(item)
-}
-// // n.set("name","naveen")
-// // console.log(n.get("run"))
-// function n(item) {
-//   // console.log(item.reduce((a,b)=>a+b))
 // }
+// // function sum(){
 
-const run1 = {
-  three:3,
-  one:1,
-  four:4,
-  two:2,
-  
-}
-var run2 = {...run1}
-// console.log(run2)
+// //     var data = document.getElementById("input1").innerText
 
-function quicksort(arr){
-  if (arr.length <= 1) {
-		return arr;
-	} else {
-    var pivot = arr[0]
-    var left = []
-    var right = []
-    for (let i = 1 ; i<arr.length; i++){
-      if(arr[i]<pivot){
-        left.push(arr[i])
-      }else{
-        right.push(arr[i])
-      }
-    }
-    // return [quicksort(...left),pivot,quicksort(...right)]
-    return quicksort(left).concat( quicksort(right),pivot);
-  }
+// //     document.getElementById("input1").innerText=data+"+"
 
-}
-var array = [10, 5, 2, 3, 7, 6, 8, 9, 1, 4];
-var arrrr = [29382,28820,280802]
-// console.log(typeof([...array,...arrrr]))
-// console.log(quicksort(array))
-
-var str = "cat"
-var arr = str.split("")
-var arr1 = [];
-var temp;
-for(let i=0 ; i< arr.length; i++){
-  var temp= arr[i]
-  arr1[i] = [];
-  
-  for(let j=0; j<arr.length; j++){
     
-    arr1[i][j]=arr[j];
-    // arr1[i][]=temp;
-  }
-  
-  
-  
-  
-}
-// console.log(arr1)
+// // }
 
-let permArr = [];
-let usedChars = [];
+// // function ans(){
+// //     var data = document.getElementById("input1").innerText
+// //     document.getElementById("para").innerText = ""
+// //     document.getElementById("para").append(eval(data))
+// //     //yeayyy
+// // }
 
-function permute(input) {
 
-  const chars = input.split("");
-  for (let i = 0; i < chars.length; i++) {
-      const ch = chars.splice(i, 1);
-      usedChars.push(ch);
-      if (chars.length == 0) {
-        permArr[permArr.length] = usedChars.join("");
-      }
-      permute(chars.join(""));
-      chars.splice(i, 0, ch);
-      usedChars.pop();
-  }
-  return permArr
-};
-// console.log(permute("hi"))
+// // function sub(){
+// //     var input1 = document.getElementById("input1").value
+// //     var input2 = document.getElementById("input2").value
 
-// const xhr = new XMLHttpRequest();
-// xhr.open("POST", "https://jsonplaceholder.typicode.com/todos");
-// xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-// const body = JSON.stringify({
-//   userId: 1,
-//   title: "Fix my bugs",
-//   completed: false
-// });
-// xhr.onload = () => {
-//   if (xhr.readyState == 4 && xhr.status == 201) {
-//     console.log(JSON.parse(xhr.responseText));
-//   } else {
-//     console.log(`Error: ${xhr.status}`);
+// //     var num1 = parseFloat(input1)
+// //     var num2 = parseFloat(input2)
+
+// //     document.getElementById("para").append(num1-num2)
+// // }
+// // function mul(){
+
+// //     var input1 = document.getElementById("input1").innerText
+// //     document.getElementById("para").append(eval(input1))
+
+// // }
+
+
+// // function push1(){
+// //     var data = document.getElementById("input1").innerText
+
+// //     var solution = data+"1"
+
+// //     document.getElementById("input1").innerText = solution
+
+    
+// // }
+// // function push2(){
+// //     var data = document.getElementById("input1").innerText
+
+// //     document.getElementById("input1").innerText=data+"2"
+// // }
+
+
+// setTimeout(
+//   function rrr(data){
+//     console.log(data)
+//     value = data
+//     alert("fuck")
+//   }, 3000); 
+
+// persons = JSON.stringify(persons)
+// fetch("", {
+//   method: "POST",
+//   body: persons,
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8"
 //   }
+// })
+//   .then((response) => response.json())
+  
+//   .then(rrr()); 
+
+// var value;
+// function run(){
+
+// var namee = document.getElementById("f").innerText
+// persons[0].name = namee
+
+  
+// // var value1
+//   // 
+//   // setTimeout(rrr, 1000);
+  
+//   // fetch('https://reqres.in/api/users')      //api for the get request
+//   // .then(response => response.json())
+//   // .then(data => console.log(data));
+// //     var name = document.getElementById("name").value
+// //     var passcode = document.getElementById("passcode").value
+// //     var para = document.createElement("p")
+// //     para.innerText = passcode
+// //     para.id = "para1"
+// //     para.style.visibility="hidden"
+// //     // para.id = "para1"
+
+// //     var div = document.createElement("div")
+// //     div.style.border="2px solid red"
+// //     div.style.height="200px"
+// //     div.style.width="200px"
+// //     div.id="div1"
+// //     div.append(name,para)
+// //     // passcode.style.visibility="hidden"
+// // // document.getElementById("passcode").style.visibility="hidden"
+// //     var button = document.createElement("button")
+// //     button.innerText= "created"
+// //     // button.addEventListener("click",run1)
+// //     button.onclick = run1
+// //     div.append(button)
+// //     document.body.append(div)
+
+
+// // var table = document.createElement("table")
+// // var row = document.createElement("tr")
+// // var col = document.createElement("td")
+// // var col1 = document.createElement("td")
+// // var col2 = document.createElement("td")
+
+// // col.innerText = "Name"
+// // col1.innerText = "class"
+// // col2.innerText = "age"
+// // row.append(col,col1,col2)
+
+// // var row1 = document.createElement("tr")
+// // var col3= document.createElement("td")
+// // var col4 = document.createElement("td")
+// // // var col5 = document.createElement("td")
+// // col3.innerText = "naveen"
+// // col4.innerText = "10"
+// // col4.setAttribute("colspan","2")
+// // // col5.innerText = "12"
+// // row1.append(col3,col4)
+// // table.append(row,row1)
+
+// // document.body.append(table)
+
+
+
+// // var forms = document.getElementsByClassName('needs-validation');
+
+// // var validation = Array.prototype.filter.call(forms, function(form) {
+// //     form.addEventListener('submit', function(event) {
+// //       if (form.checkValidity() === false) {
+// //         event.preventDefault();
+// //         // event.stopPropagation();
+// //       }
+// //       form.classList.add('was-validated');
+// //     });
+// //   });
+
+
+
+
+    
+// }
+// const empdata1to20 = [
+//   {
+//     name: "Elbertine",
+//     1: "Gissing",
+//     2: "egissing0@npr.org",
+//     4: "female",
+//     3: "$44802.77",
+//     5: "514 625 0311",
+//     6: 1,
+//     "+7":"run",
+// }
+// ]
+
+
+// // console.log(empdata1to20[0][+7])
+// // for(let i=0; i<empdata1to20.length;i++){
+// //     for(let j in empdata1to20[i]){
+// //         console.log(empdata1to20[i][j])
+// //     }
+// // }
+
+
+// // var person = new Map(empdata1to20)
+// // const persons = [
+// //   {firstname : "Malcom", lastname: "Reynolds"},
+// //   {firstname : "Kaylee", lastname: "Frye"},
+// //   {firstname : "Jayne", lastname: "Cobb"},
+// //   {
+// //     firstname: "Elbertine",
+// //     1: "Gissing",
+// //     2: "egissing0@npr.org",
+// //     4: "female",
+// //     3: "$44802.77",
+// //     5: "514 625 0311",
+// //     6: 1,
+// //     "+7":"run",
+// //   },
+// //   {
+// //     firstname: "Elbertine",
+// //     1: "Gissing",
+// //     2: "egissing0@npr.org",
+// //     4: "female",
+// //     3: "$44802.77",
+// //     5: "514 625 0311",
+// //     6: 1,
+// //     "+7":"run",
+// //   },
+// //   {
+// //     firstname: "Elbertine",
+// //     1: "Gissing",
+// //     2: "egissing0@npr.org",
+// //     4: "female",
+// //     3: "$44802.77",
+// //     5: "514 625 0311",
+// //     6: 1,
+// //     "+7":"run",
+// //   },
+// // ]
+// let persons=[
+//   {
+//     three:3,
+//     one:1,
+//     four:4,
+//     two:2,
+    
+//   }
+// ]
+// persons.map(n)
+// // console.log(n)
+// function n(item){
+//   // console.log(item)
+// }
+// // // n.set("name","naveen")
+// // // console.log(n.get("run"))
+// // function n(item) {
+// //   // console.log(item.reduce((a,b)=>a+b))
+// // }
+
+// const run1 = {
+//   three:3,
+//   one:1,
+//   four:4,
+//   two:2,
+  
+// }
+// var run2 = {...run1}
+// // console.log(run2)
+
+// function quicksort(arr){
+//   if (arr.length <= 1) {
+// 		return arr;
+// 	} else {
+//     var pivot = arr[0]
+//     var left = []
+//     var right = []
+//     for (let i = 1 ; i<arr.length; i++){
+//       if(arr[i]<pivot){
+//         left.push(arr[i])
+//       }else{
+//         right.push(arr[i])
+//       }
+//     }
+//     // return [quicksort(...left),pivot,quicksort(...right)]
+//     return quicksort(left).concat( quicksort(right),pivot);
+//   }
+
+// }
+// var array = [10, 5, 2, 3, 7, 6, 8, 9, 1, 4];
+// var arrrr = [29382,28820,280802]
+// // console.log(typeof([...array,...arrrr]))
+// // console.log(quicksort(array))
+
+// var str = "cat"
+// var arr = str.split("")
+// var arr1 = [];
+// var temp;
+// for(let i=0 ; i< arr.length; i++){
+//   var temp= arr[i]
+//   arr1[i] = [];
+  
+//   for(let j=0; j<arr.length; j++){
+    
+//     arr1[i][j]=arr[j];
+//     // arr1[i][]=temp;
+//   }
+  
+  
+  
+  
+// }
+// // console.log(arr1)
+
+// let permArr = [];
+// let usedChars = [];
+
+// function permute(input) {
+
+//   const chars = input.split("");
+//   for (let i = 0; i < chars.length; i++) {
+//       const ch = chars.splice(i, 1);
+//       usedChars.push(ch);
+//       if (chars.length == 0) {
+//         permArr[permArr.length] = usedChars.join("");
+//       }
+//       permute(chars.join(""));
+//       chars.splice(i, 0, ch);
+//       usedChars.pop();
+//   }
+//   return permArr
 // };
-// xhr.send(body);
+// console.log(permute("123456789"))
+
+// // const xhr = new XMLHttpRequest();
+// // xhr.open("POST", "https://jsonplaceholder.typicode.com/todos");
+// // xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+// // const body = JSON.stringify({
+// //   userId: 1,
+// //   title: "Fix my bugs",
+// //   completed: false
+// // });
+// // xhr.onload = () => {
+// //   if (xhr.readyState == 4 && xhr.status == 201) {
+// //     console.log(JSON.parse(xhr.responseText));
+// //   } else {
+// //     console.log(`Error: ${xhr.status}`);
+// //   }
+// // };
+// // xhr.send(body);
+// console.log("run")
+// var mat = []
+// count  = 1
+// for (let i=0; i<3; i++){
+//   mat[i] = []
+//   for(let j=0; j<3; j++){
+//       mat[i][j] = count;
+//       count++
+//   }
+// }
+// console.log(mat)
+
+// var str = "hello world".split("")
+// var j = 0;
+// for(let i = str.length-1; i>=0; i--){
+//   str[j]=str[i];
+//   j++
+
+// }
+// console.log(str.join(""))
+// var str = "naveenqaakldaadkaa";
+// var count = 0
+// for (let i=0; i<str.length; i++){
+//   if(str[i] == "a"){
+//     count++
+//   }
+// }
+// console.log(count)
+// var s = "aba"
+// var n = 1000000000000
+// var str = ''
+// var t = Math.floor(n/s.length)
+// var r = n%s.length
+// for (let i=0; i<t; i++){
+//   str += s
+// }
+// console.log(t,r)
+
+// console.log("a")
+
+
+
+
+var s = [1,2,3,4]
+var target = 5
+var count = 0
+function dhandam(count){
+   return count+1
+}
+console.log(dhandam(count))
