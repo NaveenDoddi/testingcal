7e4fc1846a4ad8f735ad11f3cf7684c3be8f21bc
naveendoddi <prasadnaveen847@gmail.com>
2023-12-26 21:37:08 +0530

    revisit posting

--- type.js
+++ type.js
@@ -1,5 +1,5 @@
 const newdata = {
-    first_name: "Jacintha",
+    first_name: "naveen",
     last_name: "Plevey",
     email: "jpleveyl@devhub.com",
     gender: "Female",
@@ -7,17 +7,25 @@ const newdata = {
     phone: "250 389 3796",
     
 }
+// json-server --watch db.json
 
 function fet(){
     fetch("http://localhost:3000/empData")
     .then(res => res.json())
     .then(data => console.log(data))
 }
-var id = 1
+
+var id = 3
 function delet(){
-    fetch('http://localhost:3000/empdata/' + id,{
-        method: "DELETE"
+    fetch("http://localhost:3000/empdata/" + id,{
+        method: "DELETE",
+
+        // body : JSON.stringify("naveen donkey"),
+        // headers: {
+        //     'Content-Type': 'application/json'
+        // },
     })
+
     id++
 }
 
@@ -27,17 +35,18 @@ function post(){
         method: "POST",
         body: JSON.stringify(newdata),
         headers: {
-            "Content-type": "application/json; charset=UTF-8"
+            "Content-type": "application/json"
         }
     });
 }
 function put(){
 
-    fetch("http://localhost:3000/empData/6", {
+    fetch("http://localhost:3000/empData/1", {
         method: "PUT",
-        body: JSON.stringify(newdata),
+        body: JSON.stringify("male"),
         headers: {
-            "Content-type": "application/json; charset=UTF-8"
+            "Content-type": "application/json"
         }
     });
 }
+
